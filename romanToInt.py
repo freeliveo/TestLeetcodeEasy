6c4bf7095e942c62053556c85da914b701dd18fc
freeliveo <1099260831@qq.com>
2021-05-08 23:13:30 +0800

    luomanumbertestfail

---
+++ romanToInt.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=13 lang=python3
+#
+# [13] 罗马数字转整数
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        I = 1
+        V = 5
+        X = 10
+        L = 50
+        C = 100
+        D = 500
+        M = 1000
+        sum = 0
+        for i in s:
+            sum+=int(i)
+        return sum 
+# @lc code=end
+
