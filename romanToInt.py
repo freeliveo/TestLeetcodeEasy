486f6ea8405bee606ea405d42d2007bf9a974779
freeliveo <1099260831@qq.com>
2021-05-10 07:16:06 +0800

    实现罗马数字转换

--- romanToInt.py
+++ romanToInt.py
@@ -7,16 +7,20 @@
 # @lc code=start
 class Solution:
     def romanToInt(self, s: str) -> int:
-        I = 1
-        V = 5
-        X = 10
-        L = 50
-        C = 100
-        D = 500
-        M = 1000
+        dict = {"I": 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000}
         sum = 0
-        for i in s:
-            sum+=int(i)
+        for i in range(len(s)-1):
+            if dict[s[i]]<dict[s[i+1]]:
+                sum-=dict[s[i]]
+            else:
+                sum+=dict[s[i]]
+        sum+=dict[s[-1]]
         return sum 
 # @lc code=end
 
